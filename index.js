d1c76a5c28493818fa7a894a51623847da09e10f
Steve <stevesantner@gmail.com>
2022-10-12 22:11:17 -0400

    commit tree c74211892ae05c1efe829dac3d6f423c7ba90fe9

---
+++ index.js
@@ -0,0 +1,45 @@
+function shout(string)  {
+    //todo
+return string.toUpperCase();
+
+"Hello!".toUpperCase(); //HELLO
+}
+
+
+function whisper(string)   {
+
+return string.toLowerCase();
+
+"hello".toLowerCase();
+
+}
+
+function logShout(spy)   {
+
+console.log(spy);
+console.log("HELLO");
+}
+
+function logWhisper(spy)   {
+
+spy.toLowerCase();
+    console.log(spy);
+    console.log("hello");
+    
+    
+    }
+
+function sayHiToHeadphonedRoommate() 
+{
+const string = undefined;
+
+
+if (string == undefined)
+    {
+        console.log("I can\'t hear you!");
+
+    }
+
+}
+
+
